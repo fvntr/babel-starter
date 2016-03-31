d086aec6c4d67af33625ba4ef91ea6e8aa09eac2
fvntr <phiven@gmail.com>
2016-03-30 19:05:00 -1000

    Add client side validation

---
+++ client/main.js
@@ -0,0 +1,9 @@
+'use strict'; 
+
+var utils = require('./utils'); 
+var iso = require('../../portable'); 
+
+console.log('Client side code started');
+console.log(iso.validateId('USER_0002'));
+
+utils.count(); 
