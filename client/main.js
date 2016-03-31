5900a7c8d208b681bb172b59da18289a5ac72f4d
fvntr <phiven@gmail.com>
2016-03-30 19:47:03 -1000

    Include package.json

--- client/main.js
+++ client/main.js
@@ -1,14 +1,21 @@
 'use strict'; 
 
+require('babel/polyfill'); 
 var utils = require('./utils'); 
 var iso = require('../../portable'); 
-var Square = require('./Square');
+var fibonacci = iso.fibonacci;
+// var Square = require('./Square');
 
 console.log('Client side code started');
 console.log(iso.validateId('USER_0002'));
 
-var s1 = new Sqaure(5); 
-var s2 = new Sqaure(); 
+// var s1 = new Sqaure(5); 
+// var s2 = new Sqaure(); 
 
-console.log(s1.area, s2.area);
-console.log(s1.sayName());
+// console.log(s1.area, s2.area);
+// console.log(s1.sayName());
+
+console.log(fibonacci.next());
+console.log(fibonacci.next());
+console.log(fibonacci.next());
+console.log(fibonacci.next(true));
